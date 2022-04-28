2c3d2c5980dedcc36f563c028fff80493013ea08
sakshipore <88977771+sakshipore@users.noreply.github.com>
2022-04-28 15:40:34 +0530

    Firestore CRUD operation done

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_01/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print("Something went wrong");
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Firestore CRUD',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: HomePage(),
+          );
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
